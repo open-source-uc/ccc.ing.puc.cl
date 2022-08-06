7d55bb4b7a8d82ba86b3c7ee4abfeb93881be5a6
Benjamín Vicente <benjavicente@uc.cl>
2022-08-06 13:50:21 -0400

    deplot action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  // TODO: cambiar a que sea variable de entorno
+  site: "benjavicente.github.io",
+  base: "webccc",
 });
