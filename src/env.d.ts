8dc5c8c65db60b596170845f995277904bb51e51
Benjamín Vicente <benjavicente@uc.cl>
2022-08-18 09:13:55 -0400

    refractor landing events

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,43 +1,44 @@
 interface ImportMetaEnv {
-  readonly PUBLIC_GOOGLE_API_KEY: string;
+	readonly PUBLIC_GOOGLE_API_KEY: string;
+	readonly BUILD_GOOGLE_API_KEY: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+	readonly env: ImportMetaEnv;
 }
 
 interface GoogleCalendarResponse {
-  summary: string;
-  description?: string | null;
-  nextPageToken?: string | null;
-  timeZone?: string | null;
-  updated?: string | null;
-  items: GoogleCalendarEvent[];
+	summary: string;
+	description?: string | null;
+	nextPageToken?: string | null;
+	timeZone?: string | null;
+	updated?: string | null;
+	items: GoogleCalendarEvent[];
 }
 
 type GoogleEventDateTime =
-  | {
-      date: string;
-      dateTime?: undefined;
-      timeZone?: undefined;
-    }
-  | {
-      date: string;
-      dateTime: string;
-      timeZone: string;
-    };
+	| {
+			date: string;
+			dateTime?: undefined;
+			timeZone?: undefined;
+	  }
+	| {
+			date: string;
+			dateTime: string;
+			timeZone: string;
+	  };
 
 interface GoogleCalendarEvent {
-  summary: string;
-  description?: string | null;
-  colorId?: string | null;
-  created: string;
-  end: GoogleEventDateTime;
-  eventType: "default" | "outOfOffice" | "focusTime";
-  htmlLink: string;
-  iCalUID?: string | null;
-  id?: string | null;
-  kind: "calendar#event";
-  location?: string | null;
-  start: GoogleEventDateTime;
+	summary: string;
+	description?: string | null;
+	colorId?: string | null;
+	created: string;
+	end: GoogleEventDateTime;
+	eventType: "default" | "outOfOffice" | "focusTime";
+	htmlLink: string;
+	iCalUID?: string | null;
+	id?: string | null;
+	kind: "calendar#event";
+	location?: string | null;
+	start: GoogleEventDateTime;
 }
--- src/lib/calendar.ts
+++ src/lib/calendar.ts
@@ -1,2 +1,15 @@
 export const googleCalendarId = "c_7h2ffgd26g1s9au82k5lovermg@group.calendar.google.com";
 export const googleAPIKey = import.meta.env.PUBLIC_GOOGLE_API_KEY;
+
+export async function getNextEventsCalendar(key: string = googleAPIKey) {
+	const now = new Date().toISOString();
+	const url = `https://www.googleapis.com/calendar/v3/calendars/${googleCalendarId}/events?timeMin=${now}&key=${key}`;
+	const response = await fetch(url);
+	const data = (await response.json()) as GoogleCalendarResponse;
+	data.items.sort((a, b) => {
+		const aDate = new Date(a.start.dateTime || a.start.date);
+		const bDate = new Date(b.start.dateTime || b.start.date);
+		return aDate.getTime() - bDate.getTime();
+	});
+	return data;
+}
