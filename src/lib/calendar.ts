69ca51888a8f2e0f5adf667ef93e1de1ba02755e
Victor <victorllagos@uc.cl>
2022-08-26 21:20:43 -0400

    fix(cursos): typo a tag

--- src/lib/calendar.ts
+++ src/lib/calendar.ts
@@ -6,6 +6,7 @@ export async function getNextEventsCalendar(key: string = googleAPIKey) {
 	const url = `https://www.googleapis.com/calendar/v3/calendars/${googleCalendarId}/events?timeMin=${now}&key=${key}`;
 	const response = await fetch(url);
 	const data = (await response.json()) as GoogleCalendarResponse;
+	console.log(data);
 	data.items.sort((a, b) => {
 		const aDate = new Date(a.start.dateTime || a.start.date);
 		const bDate = new Date(b.start.dateTime || b.start.date);
