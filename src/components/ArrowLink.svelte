8d353dd274ef76c01fbb1fbc1e0624f2c7fcaea7
nicoabarca <nicoabarca@uc.cl>
2023-01-28 18:36:26 -0300

    chore: formatter

--- src/components/ArrowLink.svelte
+++ src/components/ArrowLink.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-  import { action } from "nanostores";
+	import { action } from "nanostores";
 
-  import { classNames } from "~/lib/utils";
+	import { classNames } from "~/lib/utils";
 
-  interface $$Props {
-    class?: string;
-    href?: string;
-    action: string;
-  }
-  const { href, class: klass, action } = $$props as $$Props;
+	interface $$Props {
+		class?: string;
+		href?: string;
+		action: string;
+	}
+	const { href, class: klass, action } = $$props as $$Props;
 </script>
 
 <a class={classNames("flex items-center gap-1 text-right leading-none", klass)} {href}>
-  <span class="hover:underline">{action}</span> 
-  <span class="arrow flex-shrink-0">&rarr;</span>
+	<span class="hover:underline">{action}</span>
+	<span class="arrow flex-shrink-0">&rarr;</span>
 </a>
 
 <style lang="scss">
-  .arrow {
-    transition: transform 0.2s ease-in-out;
-    text-decoration: none;
-  }
+	.arrow {
+		transition: transform 0.2s ease-in-out;
+		text-decoration: none;
+	}
 
-  a:hover .arrow {
-    transform: translateX(calc(-1 * theme("spacing.1")));
-  }
+	a:hover .arrow {
+		transform: translateX(calc(-1 * theme("spacing.1")));
+	}
 
-  a:active .arrow {
-    transform: translateX(theme("spacing.1"));
-  }
+	a:active .arrow {
+		transform: translateX(theme("spacing.1"));
+	}
 </style>
