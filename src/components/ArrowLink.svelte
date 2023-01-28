699e27c5df0d772972079a46aee23c18730b5fee
nicoabarca <nicoabarca@uc.cl>
2023-01-28 18:01:07 -0300

    new: improve course card ui

--- src/components/ArrowLink.svelte
+++ src/components/ArrowLink.svelte
@@ -11,23 +11,22 @@
   const { href, class: klass, action } = $$props as $$Props;
 </script>
 
-<a class={classNames("flex items-center gap-1 px-1 text-right leading-none hover:underline", klass)} {href}>
-  {action}
-  <svg class="flex-shrink-0" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2.5 7H12M12 7L7 2M12 7L7 12" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-  </svg>
+<a class={classNames("flex items-center gap-1 text-right leading-none", klass)} {href}>
+  <span class="hover:underline">{action}</span> 
+  <span class="arrow flex-shrink-0">&rarr;</span>
 </a>
 
 <style lang="scss">
-  svg {
+  .arrow {
     transition: transform 0.2s ease-in-out;
+    text-decoration: none;
   }
 
-  a:hover svg {
+  a:hover .arrow {
     transform: translateX(calc(-1 * theme("spacing.1")));
   }
 
-  a:active svg {
+  a:active .arrow {
     transform: translateX(theme("spacing.1"));
   }
 </style>
