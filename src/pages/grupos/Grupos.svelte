bb4059c1c6103ea447cecc77a72399d2abf855eb
Benjamín Vicente <benjavicente@uc.cl>
2023-04-30 17:08:14 -0400

    cleanup pages directory

--- src/pages/grupos/Grupos.svelte
+++ src/pages/grupos/Grupos.svelte
@@ -1,37 +1,34 @@
 <script lang="ts">
-	import { pb } from "~/lib/pb";
+	import { pb, type Record } from "~/lib/pb";
 
-	type Group = {
+	interface Group extends Record {
 		invitation_url: string;
 		image: string;
 		description: string;
 		name: string;
-	};
+	}
 
-	const groups_promise = pb.collection("groups").getFullList();
+	const groups_promise = pb.collection("groups").getFullList<Group>();
 </script>
 
 {#await groups_promise}
 	<div class="text-center h-screen">Cargando...</div>
 {:then groups}
-	<ul class="group-grid grip-tem mx-auto flex max-w-4xl flex-col p-4 sm:grid">
+	<ul class="group-grid grip-tem mx-auto flex max-w-5xl flex-col p-4 sm:grid">
 		{#each groups as group}
 			{@const { invitation_url, image, description, name } = group}
 			<li
 				class="relative flex flex-row items-center gap-4 rounded-md bg-white p-4 shadow transition-shadow hover:shadow-lg active:shadow-sm sm:flex-col sm:text-center"
 			>
 				<div
-					class="aspect-square max-h-24 flex-shrink-0 overflow-clip rounded-lg border-2 border-stone-700 sm:max-h-48"
+					class="aspect-square max-h-24 flex-shrink-0 overflow-clip rounded-lg border-2 border-stone-700 sm:max-h-32"
 				>
 					<img src={pb.getFileUrl(group, image, { thumb: "192x192" })} alt={name} />
 				</div>
 				<div class="flex flex-grow flex-col">
-					<div class="mb-2 font-bold text-stone-900">{name}</div>
+					<div class="mb-2 text-lg leading-none font-bold text-stone-900">{name}</div>
 					<div class="flex-grow leading-tight text-stone-700">{description}</div>
-					<a href={invitation_url} class="mt-2 text-blue-500 hover:underline">
-						<span aria-hidden="true" class="absolute inset-0" />
-						Telegram
-					</a>
+					<a href={invitation_url} class="mt-2 text-blue-500 hover:underline px-1 py-2"> Telegram </a>
 				</div>
 			</li>
 		{/each}
@@ -41,7 +38,7 @@
 <style>
 	ul {
 		gap: 15px;
-		grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+		grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
 		grid-auto-rows: 1fr;
 	}
 </style>
